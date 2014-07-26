42754d582535dbd7a8063434d32c8bacd603e23d
Fredrik Folkesson <freddefolke1@gmail.com>
2014-07-26 03:39:14 +0200

    added check for internet access before trying to download

--- Sjungbok/src/com/example/sjungbok/MainActivity.java
+++ Sjungbok/src/com/example/sjungbok/MainActivity.java
@@ -2,19 +2,22 @@ package com.example.sjungbok;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	DownloadListener downloadListener;
+	TextView textView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		TextView textView = (TextView)findViewById(R.id.SongView);
-		Context context = this;
-		downloadListener = new DownloadListener(textView,context);
+		textView = (TextView)findViewById(R.id.SongView);
+		downloadSongs();
 	}
 
 	@Override
@@ -22,6 +25,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	private void downloadSongs(){
+		if(isNetworkAvailable()){
+			downloadListener = new DownloadListener(textView,this);
+		}
+		else{
+			Toast toast = Toast.makeText(this, "Aktivera Internet för att hämta sångerna", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+	}
+	
+	private boolean isNetworkAvailable() {
+		ConnectivityManager connectivityManager  = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		return activeNetworkInfo != null;
 	}
 
 }
